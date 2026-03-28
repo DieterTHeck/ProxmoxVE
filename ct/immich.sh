086ddd2d6fb2623c53562a05bed789c5456847a3
Merten Lampe <mertenlampe@MertenMacBookAir.lan>
2026-03-28 16:05:44 +0100

    feat: Add NVIDIA CUDA support and dynamic hardware detection 5

--- ct/immich.sh
+++ ct/immich.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/DieterTHeck/ProxmoxVE/immich/cuda-support/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/DieterTHeck/ProxmoxVE/immich/cuda-support/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
